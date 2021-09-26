8b927cc2a7f8864fbb1e908460e9a56c673a4ef5
Geetanjali Kanojia <71793659+gkanojia@users.noreply.github.com>
2021-09-26 15:19:34 -0400

    Add files via upload

---
+++ DroidCafe/app/src/main/java/com/example/droidcafe/OrderActivity.java
@@ -0,0 +1,18 @@
+package com.example.droidcafe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+
+    }
+}
