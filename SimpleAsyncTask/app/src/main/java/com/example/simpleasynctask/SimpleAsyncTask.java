3e1c9cb69078565d62b7c926b47f541d5cff49df
Geetanjali Kanojia <71793659+gkanojia@users.noreply.github.com>
2021-09-26 15:35:25 -0400

    Add files via upload

---
+++ SimpleAsyncTask/app/src/main/java/com/example/simpleasynctask/SimpleAsyncTask.java
@@ -0,0 +1,49 @@
+package com.example.simpleasynctask;
+
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import java.lang.ref.WeakReference;
+import java.util.Random;
+
+public class SimpleAsyncTask extends AsyncTask<Void,Void, String> {
+
+    // The TextView where we will show results
+    private WeakReference<TextView> mTextView;
+
+    // Constructor that provides a reference to the TextView from the MainActivity
+    SimpleAsyncTask(TextView tv) {
+        mTextView = new WeakReference<>(tv);
+    }
+
+    @Override
+    protected String doInBackground(Void... voids) {
+
+        // Generate a random number between 0 and 10.
+        Random r = new Random();
+        int n = r.nextInt(11);
+
+        // Make the task take long enough that we have
+        // time to rotate the phone while it is running.
+        int s = n * 200;
+
+        // Sleep for the random amount of time.
+        try {
+            Thread.sleep(s);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Return a String result.
+        return "Awake at last after sleeping for " + s + " milliseconds!";
+    }
+
+    /**
+     * Does something with the result on the UI thread; in this case
+     * updates the TextView.
+     */
+    protected void onPostExecute(String result) {
+        mTextView.get().setText(result);
+    }
+
+}
